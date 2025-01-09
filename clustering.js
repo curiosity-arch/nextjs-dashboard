ba05700b8c59c2cfd55864e42d4c5c6fe0621eef
curiosity-arch <76086739+curiosity-arch@users.noreply.github.com>
2025-01-09 21:24:32 +0700

    Delete clustering.js

--- clustering.js
+++
@@ -1,8 +0,0 @@
-const kmeans = require('ml-kmeans');
-
-function clusterData(features, numClusters = 6) {
-    const result = kmeans(features, numClusters);
-    return result.clusters;
-}
-
-module.exports = clusterData;
